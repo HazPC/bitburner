4d8176bb863c1f8cb70752ddae2d49649a5e9aad
HazPC <110977413+HazPC@users.noreply.github.com>
2022-08-11 11:57:29 +0100

    Update edit_autopilot.js

--- edit_autopilot.js
+++ edit_autopilot.js
@@ -1,5 +1,4 @@
 //line 399 above work-for-factions 
 	if (3 in unlockedSFs && !findScript('buildEmpire.js') && ns.getServer("home").maxRam > 32) {
 		 launchScriptHelper(ns, 'buildEmpire.js');
-		 //ns.run('buildEmpire.js');
 	}
