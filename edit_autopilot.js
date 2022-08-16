090f5b3e008b700fc2f488cd3caaefd7b0e8f98c
HazPC <110977413+HazPC@users.noreply.github.com>
2022-08-16 10:12:59 +0100

    Update edit_autopilot.js

--- edit_autopilot.js
+++ edit_autopilot.js
@@ -1,4 +1,8 @@
 //line 399 above work-for-factions 
-	if (3 in unlockedSFs && !findScript('buildEmpire.js') && ns.getServer("home").maxRam > 32) {
+	if (3 in unlockedSFs && !findScript('buildEmpire.js')) {
 		 launchScriptHelper(ns, 'buildEmpire.js');
 	}
+
+
+//line 422 in maybeDoCasino function before checking for player money  
+	await ns.sleep(10000);
